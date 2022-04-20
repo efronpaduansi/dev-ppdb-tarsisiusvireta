586ac62c3eae20a2708139efb86da8ffd3731019
efronpaduansi <eufrondpaduansi@gmail.com>
2022-04-20 18:16:06 +0700

    dev ppdb tarsisius vireta

---
+++ resources/views/dashboard/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; 2022 <div class="bullet"></div> SMA TARSISIUS VIRETA
+    </div>
+    <div class="footer-right">
+      v 1.0.1
+    </div>
+  </footer>
---
+++ resources/views/dashboard/sidebar.blade.php
@@ -0,0 +1,47 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <img src="{{ asset('home/assets/img/logo.png') }}" alt="" srcset="" height="50">
+        <a href="index.html">TARSISIUS VIRETA</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">TV</a>
+      </div>
+      <ul class="sidebar-menu">
+          <li class="menu-header">ADMINISTRATOR</li>
+          <li class="nav-item active">
+            <a href="#" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link" data-toggle="dropdown"><i class="fas fa-user-graduate"></i> <span>Data Siswa</span></a>
+          </li>
+          <li><a class="nav-link" href="blank.html"><i class="far fa-calendar"></i> <span>Tahun Ajaran</span></a></li>
+          <li><a class="nav-link" href="blank.html"><i class="fas fa-users"></i><span>Data Pendaftar</span></a></li>
+          <li><a class="nav-link" href="blank.html"><i class="fas fa-user-friends"></i><span>Pendaftar Diterima</span></a></li>
+          <li><a class="nav-link" href="blank.html"><i class="fas fa-file-excel"></i><span>Export Data</span></a></li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-sliders-h"></i><span>Administration</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="components-article.html">Add New User</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>
+              <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>
+              <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>
+              <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>
+              <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>
+              <li><a class="nav-link" href="components-tab.html">Tab</a></li>
+              <li><a class="nav-link" href="components-table.html">Table</a></li>
+              <li><a class="nav-link" href="components-user.html">User</a></li>
+              <li><a class="nav-link beep beep-sidebar" href="components-wizard.html">Wizard</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link" href="blank.html"><i class="fas fa-file-alt"></i><span>Documents</span></a></li>
+        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+          <a href="" class="btn btn-danger btn-lg btn-block btn-icon-split">
+            <i class="fas fa-sign-out"></i> Logout
+          </a>
+        </div>
+    </aside>
+  </div>
