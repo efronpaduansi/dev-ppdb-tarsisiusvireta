67004231359f3473c8ac252b0810b8d171cd6301
Arya Dwi Putra <aryaadwptr@gmail.com>
2022-04-25 16:47:55 +0700

    add laravel-ui authentication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function(){
     return view('dashboard.app');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
